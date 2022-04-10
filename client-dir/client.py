6478198276b70206f0808de4f0d74d8eb6983dac
n0t-4m17h <kovoor71@gmail.com>
2022-04-11 01:17:33 +1000

    made Server's infite loop + XIT cmd done

--- client-dir/client.py
+++ client-dir/client.py
@@ -2,8 +2,54 @@
 # Program written by z5361183
 # 10/04/22
 # Run; $ python3 client.py serverPort
-def hi():
-    pass
+from socket import *
+import sys
+from datetime import datetime
+import time
 
+def XIT(clientSocket, serverPort):
+    clientSocket.sendto("Curr socket has exited".encode("utf-8"), ('localhost', int(serverPort)))
+    clientSocket.close()
+    sys.exit("Goodbye")
+    
 
 # let the OS pick a random Client Port -> "sock.bind(('localhost', 0))", selected port is in "sock.getsockname()"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit('Execute program as such: $python3 client.py <server_port>')
+
+    serverPort = int(sys.argv[1])
+    WAIT_TIME_NRML = 5 # wait time for non-image commands
+    WAIT_TIME_IMG = 15 # wait time for image transfers
+
+    clientSocket = socket(AF_INET, SOCK_DGRAM) # sock_dgram == UDP socket
+    # Set the timeout time if packet is taking too long/lost
+    # clientSocket.settimeout(WAIT_TIME)
+
+    while 1:
+        # try:
+            # simplify datetime to just HH:MM:SS, 24hr time
+            timestamp = str(datetime.today())[11:19]
+
+            command = str(input("Enter username: "))
+            if command == "XIT":
+                XIT(clientSocket, serverPort)
+
+            # ".encode("utf-8")" turns the message into a "bytes-like object", from a str. ".decode("utf-8") does the opposite
+            clientSocket.sendto(command.encode("utf-8"), ('localhost', int(serverPort)))
+            t1 = time.time()
+
+            # Waiting for Server Response --> This is where program pauses (w/out exception) if packet is lost
+            # pingEchoCommand, serverAddress = clientSocket.recvfrom(2048)
+            t2 = time.time()
+
+        # LEGACY: except socket.timeout: # not working, baseclass issue
+        # except:
+        #     # Assume the packet was lost (cause UDP), hence "timed out"
+        #     print("Packet timeout: resending timed-out packet")
+        #     continue
+    
+    # print(f"Min RTT: {min_rtt}\nMax RTT: {max_rtt}\nAvg RTT: {avg_rtt}")
+
+    clientSocket.close()
--- server.py
+++ server.py
@@ -2,6 +2,10 @@
 # Program written by z5361183
 # 10/04/22
 # Run; $ python3 server.py serverPort 
+from socket import *
+import sys
+from datetime import datetime
+import time
 
 
 ###########################
@@ -16,24 +20,63 @@ dataStore = {
 ### Helper Fncs ###
 ###################
 
-# Creates a new user (Upon detection of a new login)
-def create_new_user(username, password, activeStatus):
-    # newUser = {"username": poo, "password": pee, "isActive": True}
-    # dataStore['users'].append(newUser)
-    pass
-
 # Wipes the whole data store clean (used when Server is killed)
-def dataStore_clear():
+def dataStoreClear():
     # dataStore['users'] = []
     # dataStore['threads'] = []
     pass
 
+# Creates a new user (Upon detection of a new login)
+def createNewUser(username, password, isActive):
+    # newUser = {"username": poo, "password": pee, "isActive": True}
+    # dataStore['users'].append(newUser)
+    pass
+
+# Checks if the given Username exists
+def userIsNew():
+    pass
+
+def changeUserActive():
+    pass
 
 ##################
 #### MAIN Fnc ####
 ##################
-def main():
-
-
 if __name__ == "__main__":
-    main()
+    if (len(sys.argv) != 2):
+        sys.exit("Execute program as such: $python3 server.py <server_port>")
+    
+    serverPort = int(sys.argv[1])
+
+    serverSocket = socket(AF_INET, SOCK_DGRAM) 
+    serverSocket.bind(('localhost', serverPort))
+    # serverSocket.listen(1) # only for TCP ??
+
+    while 1:
+        print("Server waiting for clients...")
+        message, clientAddr = serverSocket.recvfrom(2048)
+        print("Client authenticating")
+        while 1:
+            # messagePass, clientAddr = serverSocket.recvfrom(2048)
+            try:
+                # analyse message
+                alterMsg = (message.decode("utf-8")).upper()
+                print(alterMsg)
+                message, clientAddr = serverSocket.recvfrom(2048)
+                if userIsNew(alterMsg) is True:
+                    serverSocket.sendto("PASSWORD", clientAddr) # Wait for password next
+                #   passMsg, clientAddr = serverSocket.recvfrom(2048)
+                #   pass = (passMsg.decode("utf-8"))
+                #   createNewUser(alterMsg, pass, True)
+                elif "has exited" in message.decode("utf-8"):
+                    # print(f"{user} exited")
+                    print("Deez exited")
+                    break
+
+            # Upon "ctrl + c"
+            except KeyboardInterrupt:
+                print("\nKilling server...")
+                dataStoreClear()
+                serverSocket.close()
+                break
+
